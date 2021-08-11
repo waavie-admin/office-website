98d9439695152aa4d80bacdcc9c9c60db3f95d23
Harish <harish@waavie.com>
2021-08-11 23:12:01 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from "./components/Navbar/Navbar"
+
+export default function App() {
+  return (
+
+    <Navbar / >
+  );
+}
